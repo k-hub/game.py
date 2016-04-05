9f916017835bef024698d555308db4229c013811
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 14:40:01 -0700

    Greet User

---
+++ game.py
@@ -0,0 +1,31 @@
+# from random import randint
+
+# name = raw_input("Hi, what's your name? ")
+# print "%s, let's play a guessing game!" % name
+
+# rand = randint(1, 100)
+# print "random number:", rand
+
+# guess =int(raw_input("I'm thinking of a number between 1 and 100. Guess my number: "))
+
+
+# while True:
+#     if guess > rand:
+#         print "Your  guess is too high, try again."
+#         guess = int(raw_input("Your guess? "))
+#     elif guess < rand:
+#         print "Your guess is too low, try again."
+#         guess = int(raw_input("Your guess? "))
+#     else:
+#         print "Congratulations, %s, you guessed my number!" % name
+#         break
+       
+
+from random import randint
+rand = randint(1, 100)
+print "random number:", rand
+
+name = raw_input("Hi, what's your name? ")
+print "%s, let's play a guessing game!" % name
+
+
